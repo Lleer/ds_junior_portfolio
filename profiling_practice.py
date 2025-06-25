d8cf3ef8bcee25a7cd0b23ffccc969d706e6d02e
Lleer <143787452+Lleer@users.noreply.github.com>
2025-06-25 14:12:55 +0300

    feat: add profiling practice

---
+++ profiling_practice.py
@@ -0,0 +1,17 @@
+import random
+import cProfile
+
+def sum_list(lst):
+    total = 0
+    for x in lst:
+        total += x
+    return total
+
+def sort_big_list(lst):
+    return sorted(lst)
+
+lst = list(range(1, 10001))
+big_lst = [random.randint(1, 100000) for _ in range(10000)]
+
+cProfile.run('sum_list(lst)')
+cProfile.run('sort_big_list(big_lst)')
